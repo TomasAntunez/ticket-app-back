183753c8f3bdec4322c27951aa101e0c6ad78fa0
TomasAntunez <chuliantunez@gmail.com>
2023-04-03 18:10:15 -0300

    fix import socket

--- models/Server.js
+++ models/Server.js
@@ -3,7 +3,7 @@ const express  = require('express');
 const http     = require('http');
 const socketio = require('socket.io');
 const path     = require('path');
-const Sockets  = require('./sockets');
+const Sockets  = require('./Sockets');
 
 
 class Server {
